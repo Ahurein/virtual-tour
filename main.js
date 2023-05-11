004baba84a960f12fc06dd2ec2e14ef020c456e3
Ahurein Ebenezer <ahureinebenezer@gmail.com>
2023-05-11 00:48:41 +0000

    remove comments

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-// const panoramaImage = new PANOLENS.ImagePanorama("images/image1.jpeg");
-
-
-// const viewer = new PANOLENS.Viewer({
-//   container: imageContainer,
-//   autoRotate: true,
-//   autoRotateSpeed: 0.3,
-//   controlBar: false,
-// });
-
-// viewer.add(panoramaImage);
-
-
 const imageContainer = document.querySelector(".image-container");
 
 const front = new PANOLENS.ImagePanorama('/images/takoradi_front.jpg');
